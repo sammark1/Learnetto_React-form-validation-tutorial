9196a6592543acd3ae3f05bb5d726e29278965ee
Sammark1 <samaustinmark@gmail.com>
2022-09-25 13:57:55 -0400

    form HTML content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-// import Form from './Form.js';
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./Form.js";
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+        <img src={logo} className="App-logo" alt="logo" />
         <h2>React Form Validation Demo</h2>
+        <Form />
       </div>
     </div>
   );
